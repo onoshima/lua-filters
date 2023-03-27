9c1a1bc5b8d53492d303948aea42b920b86d29d8
onoshima <onoshima.t@gmail.com>
2023-03-27 17:48:57 +0900

    custom-figureにwidth変更を追加

--- lua/latex-custom-figure.lua
+++ lua/latex-custom-figure.lua
@@ -1,7 +1,12 @@
-local width = "width=0.5\\linewidth"
+local default_width = "width=0.5\\linewidth"
 
 local function extract_figinfo(figure)
   local result = {}
+  if figure.c[1].c[1].attr.attributes["width"] then
+    result["width"] = figure.c[1].c[1].attr.attributes["width"].."\\linewidth"
+  else
+    result["width"] = default_width
+  end
   result["label"] = figure.attr.identifier
   result["src"] = figure.c[1].c[1].src
   result["caption"] = figure.c[1].c[1].caption[1].text
@@ -14,7 +19,7 @@ local function get_latex_fig(figinfo)
   \centering
   \hypertarget{]]..figinfo["label"]..[[}{%
     \caption{]]..figinfo["caption"].."}\\label{"..figinfo["label"]..[[}
-    \includegraphics[]]..width.."]{"..figinfo["src"]..[[}
+    \includegraphics[]]..figinfo["width"].."]{"..figinfo["src"]..[[}
   }
 \end{figure}]]
 end
